c8865abd7010c8f514046cdef06d52255837de99
Joseph Callen <jcpowermac@gmail.com>
2016-11-17 16:15:27 -0500

    Changed to yaml, easier to read

--- images/layers.py
+++ images/layers.py
@@ -4,6 +4,8 @@ from subprocess import Popen, PIPE
 import requests
 import pprint
 import json
+import yaml
+
 
 def gettoken():
 
@@ -12,6 +14,27 @@ def gettoken():
     rc = p.returncode 
     return output.rstrip('\n')
 
+
+
+def aosget_text(url, token):
+
+    headers = {'Authorization': 'Bearer %s' % token, 'Accept': 'application/yaml'}
+    req = requests.get(url, headers=headers, verify=False)
+
+    if req.status_code == 200:
+        text = req.text
+        return text 
+    return ""
+
+def aosget_yaml(url, token):
+
+    headers = {'Authorization': 'Bearer %s' % token, 'Accept': 'application/yaml'}
+    req = requests.get(url, headers=headers, verify=False)
+
+    if req.status_code == 200:
+        text = req.text
+        return yaml.load(text)
+    return {}
 
 def aosget(url, token):
 
@@ -31,8 +54,8 @@ def main():
 
     url = "https://origin-master1.virtomation.com:8443/oapi/v1/images"
 
-    results = aosget(url, token)
-    pp.pprint( json.dumps(results['items'][5]  ) )
+    results = aosget_text(url, token)
+    print results
 
 if __name__ == '__main__':
     main()
